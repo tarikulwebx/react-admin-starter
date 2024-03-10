21d6a86e8e8c2b193504b7bdd1606b6c5bb5a601
Md. Tarikul Islam <tarikulwebx@gmail.com>
2024-03-10 11:37:24 +0600

    Logo size

--- src/components/sidebar/SidebarHeader.jsx
+++ src/components/sidebar/SidebarHeader.jsx
@@ -5,7 +5,7 @@ const SidebarHeader = () => {
         <div className=" mb-4 flex h-16 items-center border-b border-gray-100 px-3.5">
             <h1 className="text-[22px] font-bold text-primary">
                 <Link to={"/"} className="flex flex-row items-center gap-2">
-                    <img src="/logo.png" width={40} height={40} alt="logo" className="h-10 w-10" />
+                    <img src="/logo.png" width={40} height={40} alt="logo" className="h-11 w-11" />
                     MMS Master
                 </Link>
             </h1>
